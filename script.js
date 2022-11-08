7fe22a01e70b8abd6a3506c9e570fbd30d1a25e4
Dmitry <bot2biz@gmail.com>
2022-11-08 19:35:27 +0200

    Четвертій коммит

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let a =2;
+let money = +prompt("Ваш бюджет на месяц?");
+let date = prompt("Введите дату в формате YYYY-MM-DD");
 
-console.log(a);
+let expences1 = prompt("Введите обязательную статью расходов в этом месяце");
+let expences2 = prompt("Во сколько обойдется?");
+const appData = {
+    "бюджет" : money,
+    "данные времени" : date,
+    expence : {
+        expences1 : expences2
+    },
+    optionalExpenses : "",
+    income : [],
+    savings : false
+}
+let oneDayBudg = money/30;
+alert(oneDayBudg);
