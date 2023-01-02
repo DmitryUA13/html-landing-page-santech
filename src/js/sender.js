7a7e994315afc3a11d80c4988d64c09df6d28296
DmitryFilimonov_Ukraine <34549353+DmitryUA13@users.noreply.github.com>
2023-01-02 17:59:35 +0200

    Update sender.js

--- src/js/sender.js
+++ src/js/sender.js
@@ -16,5 +16,5 @@ let data = {
 }
 
 xhr.open('POST', url );
-xhr.setRequestHeader('X-Viber-Auth-Token', '502e748825a7e3b8-ccd095a47f2cdba5-29d9a2c12ec4cdc5');
+xhr.setRequestHeader('X-Viber-Auth-Token', '');
 xhr.send(data);
